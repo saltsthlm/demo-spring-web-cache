7dc6964495daf4b48e4e4d6250e37e87de371029
alexmiloeski <a.miloeski@gmail.com>
2024-09-23 08:40:55 +0200

    feat: set up ehcache

---
+++ src/main/java/org/example/demospringwebcache/CacheEventLogger.java
@@ -0,0 +1,12 @@
+package org.example.demospringwebcache;
+
+import org.ehcache.event.CacheEvent;
+import org.ehcache.event.CacheEventListener;
+
+public class CacheEventLogger implements CacheEventListener<Object, Object> {
+
+    @Override
+    public void onEvent(CacheEvent<?, ?> cacheEvent) {
+        System.out.println("CacheEvent: k=" + cacheEvent.getKey() + "; old=" + cacheEvent.getOldValue() + "; new=" + cacheEvent.getNewValue());
+    }
+}
